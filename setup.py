62e5153d7dc1ce1c4ebdb709a229d46657e3e410
Kerwin0504 <142535824+Kerwin0504@users.noreply.github.com>
2023-08-23 23:35:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Kerwin_RA',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        
+        
+        'pandas',
+        'geopandas',
+        'pyreadr',
+        'matplotlib',
+        'shapely.geometry',
+        'mpl_toolkits.mplot3d',
+        'mpl_toolkits.mplot3d.art3d',
+        'numpy',
+        'matplotlib.animation',
+        'IPython.display'
+        
+        
+        
+        ],
+    author='Jiangquan Dai',
+    author_email='m15914863239@163.com',
+    url='https://github.com/junlingm/EpiVisPy',
+    description='A package to generate a 3D animation over a 2D map.',
+    license='MIT',
+    keywords="animation geospatial",
+)
 
